487f273adb11d646ffa586e2e43a2ab4be104772
hafizbima <mas.bima.hafiz@gmail.com>
2023-10-31 14:26:09 +0700

    modul6

---
+++ modul6/components/index.js
@@ -0,0 +1,10 @@
+
+import Header from "./header";
+import CategoryButton from "./category-button";
+import Categories from "./categories";
+import NewsItem from "./news-item";
+
+export { Header, CategoryButton, Categories, NewsItem };
+
+
+
